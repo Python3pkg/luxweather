4632168fc6e8a945e78df34f9cd01dead16336a1
c0ding <me@martinsimon.me>
2015-01-24 14:55:08 +0100

    v.1.0.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 
 setup(
     name = 'luxweather',
-    version = '1.0',
+    version = '1.0.1',
     url = 'https://github.com/c0ding/LuxWeather',
     download_url = 'https://github.com/c0ding/LuxWeather/archive/master.zip',
     author = 'c0ding',
